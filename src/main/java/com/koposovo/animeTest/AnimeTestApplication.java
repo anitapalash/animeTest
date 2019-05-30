a38b078dd22b6898ed03c042301f3f6ed6a372df
anitapalash <anitapalash98@gmail.com>
2019-05-30 14:51:50 +0300

    ОНО ЗАПУСТИЛОСЬ

--- src/main/java/com/koposovo/animeTest/AnimeTestApplication.java
+++ src/main/java/com/koposovo/animeTest/AnimeTestApplication.java
@@ -2,22 +2,36 @@ package com.koposovo.animeTest;
 
 import javafx.application.Application;
 
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 public class AnimeTestApplication extends Application {
+	private ConfigurableApplicationContext context;
+	private Parent rootNode;
+
+	@Override
+	public void init() throws Exception {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(AnimeTestApplication.class);
+		context = builder.run(getParameters().getRaw().toArray(new String[0]));
+	}
+
 	public static void main(final String[] args) {
-		SpringApplication.run(AnimeTestApplication.class);
+		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
         SpringStageLoader.loadMain().show();
 	}
+
+
 }
